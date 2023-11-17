88728adcefca7cc6267324eadd3f1933579664fa
Janarthanan003 <ajanarthanan06@gmail.com>
2023-11-17 16:05:59 +0530

    Task-9

---
+++ src/factorialofagivennumber/FactorialNumber.java
@@ -0,0 +1,11 @@
+package factorialofagivennumber;
+import java.util.Scanner;
+public class FactorialNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		for(int a=5;a<=120;a++);
+		System.out.println("a");
+	}
+}
